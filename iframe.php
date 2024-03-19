45569db2516cf1ed4e0e87fc16bd612e71992be0
Michal Lauko <lauko@zeroesones.cz>
2024-03-19 11:58:17 +0100

    implementations

---
+++ iframe.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        html {
+            overflow-x: hidden;
+        }
+
+        .menu {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100vw;
+            height: 80px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            z-index: calc(infinity);
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: lightgray;
+        }
+
+        footer {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100vw;
+            height: 320px;
+            position: relative;
+            z-index: calc(infinity);
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: grey;
+        }
+    </style>
+</head>
+
+<body style="margin:0;padding:0;height:200dvh;">
+    <div class="menu">Partner menu/header</div>
+    <iframe src="https://www.fotokoutek-vecerek.cz/promo" width="100%" height="100%" style="min-height: 100dvh;"
+        frameborder="0"></iframe>
+    <footer>
+        Partner footer
+    </footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -8,43 +8,33 @@
     <style>
         html {
             overflow-x: hidden;
+            font-family: Arial, Helvetica, sans-serif;
         }
 
-        .menu {
+        .split {
             display: flex;
+            flex-direction: row;
             align-items: center;
             justify-content: center;
-            width: 100vw;
-            height: 80px;
+            gap: 64px;
             position: fixed;
             top: 0;
             left: 0;
-            z-index: calc(infinity);
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: lightgray;
-        }
-
-        footer {
-            display: flex;
-            align-items: center;
-            justify-content: center;
             width: 100vw;
-            height: 320px;
-            position: relative;
-            z-index: calc(infinity);
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: grey;
+            height: 100dvh;
         }
     </style>
 </head>
 
 <body style="margin:0;padding:0;height:200dvh;">
-    <div class="menu">Partner menu/header</div>
-    <iframe src="https://www.fotokoutek-vecerek.cz/promo" width="100%" height="100%" style="min-height: 100dvh;"
-        frameborder="0"></iframe>
-    <footer>
-        Partner footer
-    </footer>
+    <div class="split">
+        <a href="/iframe.php">
+            Verze iframe
+        </a>
+        <a href="/iframe.php">
+            Verze php
+        </a>
+    </div>
 </body>
 
 </html>
